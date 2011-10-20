7420d5c1cbf301c23177230e414f2b1189f4a552
Paul Dragoonis <dragoonis@gmail.com>
2011-10-20 22:43:09 +0100

    Removing ::add(), adding ->getConnection(), ->factory(), ::create(), PHPDOC'ing everything.

--- DataSource.php
+++ DataSource.php
@@ -7,30 +7,55 @@
  */
 class PPI_DataSource {
 
-	protected static $_sources = array();
-	protected static $_connections = array();
+	/**
+	 * List of configuration sets
+	 * 
+	 * @var array
+	 */
+	protected $_config = array();
+	
+	/**
+	 * List of connections to return via singleton-like
+	 * 
+	 * @var array
+	 */
+	protected $_handles = array();
 
-	function __construct() {}
+	/**
+	 * The constructor, taking in options which are currently
+	 * 
+	 * @param array $options
+	 */
+	function __construct(array $options = array()) {
+		$this->_config = $options;
+	}
+	
+	/**
+	 * Create a new instance of ourself.
+	 * 
+	 * @static
+	 * @param array $options
+	 * @return PPI_DataSource
+	 */
+	static function create(array $options = array()) {
+		return new self($options);
+	}
 
-	static function factory($key) {
+	/**
+	 * The DataSource Factory - this is where we manufacture our drivers
+	 * 
+	 * @throws PPI_Exception
+	 * @param string $key
+	 * @return object
+	 */
+	function factory(array $options) {
 
-		// Connection Caching
-		if(isset(self::$_connections[$key])) {
-			return self::$_connections[$key];
-		}
-
-		// Check that we asked for a valid key
-		if(!isset(self::$_sources[$key])) {
-			throw new PPI_Exception('Invalid DataSource Key: ' . $key);
-		}
-
-		// See if re recognise our data source's type
-		$options = self::$_sources[$key];
-
+		// Apply our default prefix
 		if(!isset($options['prefix'])) {
 			$options['prefix'] = 'PPI_DataSource_';
 		}
 
+		// Lets get our suffix, to load up the right adapter, (PPI_DataSource_[PDO|Mongo])
 		if($options['type'] === 'mongo') {
 			$suffix = 'Mongo';
 		} elseif(substr($options['type'], 0, 4) === 'pdo_') {
@@ -39,17 +64,39 @@ class PPI_DataSource {
 			$suffix = $options['type'];
 		}
 
+		// Lets instantiate up and get our  driver
 		$adapterName = $options['prefix'] . $suffix;
 		$adapter     = new $adapterName();
 		$driver      = $adapter->getDriver($options);
-		
-		self::$_connections[$key] = $driver; // Connection Caching
+
 		return $driver;
-
 	}
+	
+	/**
+	 * Return the connection from the factory
+	 * 
+	 * @throws PPI_Exception
+	 * @param string $key
+	 * @return object
+	 */
+	function getConnection($key) {
+		
+		// Connection Caching
+		if(isset($this->_handles[$key])) {
+			return $this->_handles[$key];
+		}
+		
+		// Check that we asked for a valid key
+		if(!isset($this->_config[$key])) {
+			throw new PPI_Exception('Invalid DataSource Key: ' . $key);
+		}
+		
+		$conn = $this->factory($this->_config[$key]);
+		
+		// Connection Caching
+		$this->_handles[$key] = $conn;
 
-	static function add($key, array $options) {
-		self::$_sources[$key] = $options;
+		return $conn;
 	}
 
 }
